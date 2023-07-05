5e0d7f1cafc9ef9348cde82add27a80a7edd62f3
Islam Gad <islam.gad@elsewedy.com>
2023-07-05 16:59:20 +0300

    Initial commit

---
+++ src/main/java/com/islamGad/orderManagementBackEnd/OrderManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.islamGad.orderManagementBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/islamGad/orderManagementBackEnd/OrderManagementBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islamGad.orderManagementBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
